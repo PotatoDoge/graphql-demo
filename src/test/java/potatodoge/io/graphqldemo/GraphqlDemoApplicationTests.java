0f42ff2b760a141c63161987164f92f548b578b6
bprado <bastian.prado.v@gmail.com>
2023-06-24 11:59:57 -0700

    Initial commit

---
+++ src/test/java/potatodoge/io/graphqldemo/GraphqlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package potatodoge.io.graphqldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
